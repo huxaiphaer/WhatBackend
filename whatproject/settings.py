fdd8a611e834474ba59c9eb2f94d6f0cf9c8aeee
Huzaifah <huzaifah.lutaaya@lawpilots.com>
2024-12-19 16:20:59 +0100

    Debug Divion envs

--- whatproject/settings.py
+++ whatproject/settings.py
@@ -23,10 +23,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print("Available environment variables:", os.environ.keys())
-print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
-print("DEFAULT_DATABASE_DSN:", os.environ.get('DEFAULT_DATABASE_DSN'))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
